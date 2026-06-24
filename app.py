f102234107a344e6893e51699818554bc8b01fb8
njc33 <njceraso@gmail.com>
2026-06-24 10:27:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown(
 )
 
 # === OneDrive EMBED LINK (working embed link) ===
-embed_src = "https://1drv.ms/x/c/13C7E9465F9473DD/IQT8CnkpzF0wQ75QWftP-BC8AboZVps3bpDok7h1RQCo8_M"
+embed_src = "https://1drv.ms/x/c/13C7E9465F9473DD/IQDqHyCCQIPGQ4OvFquRDo2NAcFaXizA-17m1OFlf68Hc-A?e=JskLLk"
 
 # === Fixed viewport size for the embedded workbook ===
 container_width = 1200
